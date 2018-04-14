a61170ea925d34ecc53b39fc82130ecc2e6c841c
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-04-14 00:38:14 -0300

    changed print procedures to macros

--- lempel-ziv/lz78-compress.asm
+++ lempel-ziv/lz78-compress.asm
@@ -1,3 +1,38 @@
+# MACROS
+	# Print value from register address in hexadecimal format
+	.macro phex %reg
+		move $t9, $a0
+		li   $v0, 34
+		move   $a0, %reg
+		syscall
+		move $a0, $t9
+	.end_macro
+
+	# Print null terminated (.asciiz) string
+	.macro pstr %str
+		move $t9, $a0
+		li   $v0, 4
+		la   $a0, %str
+		syscall
+		move $a0, $t9
+	.end_macro
+
+	# Print a \n to the console
+	.macro newl
+		move $t9, $a0
+		li   $v0, 11
+		li   $a0, '\n'
+		syscall
+		move $a0, $t9
+	.end_macro
+
+	# Terminate the program
+	.macro exit
+		pstr endProgram
+		li   $v0, 10
+		syscall
+	.end_macro
+	
 .data
 
 	beginMain:  .asciiz "Starting main procedure\n"
@@ -5,32 +40,10 @@
 	newLine:    .asciiz "\n"
 	
 .text
-	main:	# Main procedure
-		la   $a0, beginMain
-		jal  print_string
+	# Main procedure
+	main:
+		pstr beginMain
 
 		# Your code goes in here
 		
-		j exit
-
-	print_hex: # Print value from %a0 address in hexadecimal format
-		li   $v0, 34
-		syscall
-		jr   $ra
-
-	print_string: # Print null terminated string from $a0 address
-		li   $v0, 4
-		syscall
-		jr   $ra
-
-	new_line:
-		li   $v0, 11
-		li   $a0, '\n'
-		syscall
-		jr   $ra
-
-	exit:	# Terminate the program
-		la   $a0, endProgram
-		jal  print_string
-		li   $v0, 10
-		syscall
+		exit
