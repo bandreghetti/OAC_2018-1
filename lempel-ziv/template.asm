8556091cc789055ec0a7615fd87bdc4fd7797632
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-04-13 22:49:15 -0300

    added new functions to the template
    also copied them to the lz78-compress.asm file

--- lempel-ziv/template.asm
+++ lempel-ziv/template.asm
@@ -1,8 +1,10 @@
 .data
 
-	beginMain: .asciiz "Starting main procedure\n"
-
-	endProgram: .asciiz "Exiting\n"
+	string:     .asciiz "String: "
+	hex:        .asciiz "Hex:    "
+	beginMain:  .asciiz "Starting main procedure"
+	endProgram: .asciiz "Exiting"
+	newLine:    .asciiz "\n"
 	
 .text
 	main:	# Main procedure
@@ -10,14 +12,45 @@
 		jal  print_string
 
 		# Your code goes in here
-
+		
 		j exit
 
-	print_string:  # Print null terminated string from $a0 address
+	print_hex:
+		# Save $v0 to the stack
 		addi $sp, $sp, -4
 		sw   $v0, 0($sp)
+		# Effectively print the hexadecimal value
 		li   $v0, 4
+		move $t1, $a0
+		la   $a0, hex
 		syscall
+		li   $v0, 34
+		move $a0, $t1
+		syscall
+		li   $v0, 4
+		la   $a0, newLine
+		syscall
+		# Restores initial register states
+		move $a0, $t1
+		lw   $v0, 0($sp)
+		addi $sp, $sp, 4
+		jr   $ra
+
+	print_string:  # Print null terminated string from $a0 address
+		# Save $v0 to the stack
+		addi $sp, $sp, -4
+		sw   $v0, 4($sp)
+		# Effectively print the string
+		li   $v0, 4
+		move $t1, $a0
+		la   $a0, string
+		syscall
+		move $a0, $t1
+		syscall
+		la   $a0, newLine
+		syscall
+		# Restores initial register states
+		move $a0, $t1
 		lw   $v0, 0($sp)
 		addi $sp, $sp, 4
 		jr   $ra
@@ -25,6 +58,5 @@
 	exit:	# Terminate the program
 		la   $a0, endProgram
 		jal  print_string
-		
 		li   $v0, 10
 		syscall
