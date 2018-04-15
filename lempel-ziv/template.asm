d6b9bc8325aeda628b27002f9ee1bd30c7a1c8c6
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-04-15 13:14:37 -0300

    created printutils.asm file with printing macros
    also modified template.asm to use .include directive

--- lempel-ziv/template.asm
+++ lempel-ziv/template.asm
@@ -1,43 +1,9 @@
-# MACROS
-	# Print value from register address in hexadecimal format
-	.macro phex %reg
-		move $t9, $a0
-		li   $v0, 34
-		move   $a0, %reg
-		syscall
-		move $a0, $t9
-	.end_macro
+.include "printutils.asm"
 
-	# Print null terminated (.asciiz) string
-	.macro pstr %str
-		move $t9, $a0
-		li   $v0, 4
-		la   $a0, %str
-		syscall
-		move $a0, $t9
-	.end_macro
-
-	# Print a \n to the console
-	.macro newl
-		move $t9, $a0
-		li   $v0, 11
-		li   $a0, '\n'
-		syscall
-		move $a0, $t9
-	.end_macro
-
-	# Terminate the program
-	.macro exit
-		pstr endProgram
-		li   $v0, 10
-		syscall
-	.end_macro
-	
 .data
 
 	beginMain:  .asciiz "Starting main procedure\n"
 	endProgram: .asciiz "Exiting\n"
-	newLine:    .asciiz "\n"
 	
 .text
 	# Main procedure
