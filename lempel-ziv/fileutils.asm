b5a892e7a5b7f1e4bccf86b667a2cb4403b7400c
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-04-23 00:21:14 -0300

    finished dict output write procedure
    also added intwrite macro to write integer to file

--- lempel-ziv/fileutils.asm
+++ lempel-ziv/fileutils.asm
@@ -1,3 +1,6 @@
+.data
+	byteBuffer:  .space 1
+
 .macro fopen %fp, %filename, %mode
 	li $v0, 13 # File Open
 	la $a0, %filename
@@ -19,4 +22,56 @@
 	la $a1, %buffer
 	li $a2, %numbytes
 	syscall
+.end_macro
+
+.macro fwrite %buffer, %numbytes, %fp
+	li $v0, 15 # File Write
+	lw $a0, %fp
+	la $a1, %buffer
+	li $a2, %numbytes
+	syscall
+.end_macro
+
+.macro intwrite %reg, %fp
+	addi $sp, $sp, -12 # Int Write
+	sw $t6, 4($sp)
+	sw $t7, 8($sp)
+	sw $t5, 12($sp)
+	move $t6, %reg
+	li $v0, 15
+	lw $a0, %fp
+	la $a1, byteBuffer
+	li $a2, 1
+	li $t7, 1000000000
+	move $t5, $zero
+	whileIntWrite:
+		beqz $t7, breakIntWrite
+		div  $t9, $t6, $t7
+		beqz $t9, remZero
+		notLeftZero:
+		addi $t9, $t9, 48
+		li $t5, 1
+		sb   $t9, byteBuffer
+		addi $t9, $t9, -48
+		syscall
+		li   $v0, 15
+		j avoidBranch
+		remZero:
+		bnez $t5, notLeftZero
+		avoidBranch:
+		mul  $t8, $t7, $t9
+		sub  $t6, $t6, $t8
+		li   $t8, 10
+		div  $t7, $t7, $t8
+		j whileIntWrite
+	breakIntWrite:
+	bnez $t5, avoidZero
+	li $t9, 48
+	sb $t9, byteBuffer
+	syscall
+	avoidZero:
+	lw $t5, 12($sp)
+	lw $t7, 8($sp)
+	lw $t6, 4($sp)
+	addi $sp, $sp, +8	
 .end_macro
--- lempel-ziv/lz78-compress.asm
+++ lempel-ziv/lz78-compress.asm
@@ -2,20 +2,26 @@
 
 .data
 
-	beginMain:        .asciiz "Starting main procedure\n"
-	inputFileMessage: .asciiz "Please enter the name of the file you want to compress:\n"
-	inputFileName:    .space  80
+	outputCharBuffer:    .space  1
+	newline:            .asciiz "\n"
+	colon:              .asciiz ":"
+	space:              .asciiz " "
+	comma:              .asciiz ","
+	outputDictFileName: .asciiz "out/dict.txt"
+	beginMain:          .asciiz "Starting main procedure\n"
+	inputFileMessage:   .asciiz "Please enter the name of the file you want to compress:\n"
+	inputFileName:      .space  80
 	.align 2
-	inputFP:          .word   0
-	compressedData:   .word   0
-	countBuffer:      .space  1024
-	fileSize:         .word   0
+	FP:            .word   0
+	compressedData:     .word   0
+	countBuffer:        .space  1024
+	fileSize:           .word   0
 	.align 2
-	outputDict:       .word   0
+	outputDict:         .word   0
 	.align 2
-	outputChars:      .word   0
+	outputChars:        .word   0
 	.align 2
-	nextChar:         .space  1
+	nextChar:           .space  1
 
 .text
 	# Main procedure
@@ -24,27 +30,75 @@
 		pstr  inputFileMessage
 		kbin  inputFileName
 		
-		fopen inputFP, inputFileName, 0
+		fopen FP, inputFileName, 0
 		jal countBytes
 		sw $v0, fileSize
-		fclose inputFP
+		fclose FP
 		
 		sll $t0, $v0, 2
 		malloc outputDict, $t0
 		malloc outputChars, $v0
 		
-		fopen inputFP, inputFileName, 0
+		fopen FP, inputFileName, 0
 		jal compressData
-		fclose inputFP
+		fclose FP
 		
-		
+		jal writeDictFile
 		
 		exit
+
+	# $s0 = address to the output dictionary indexes
+	# $s1 = address to the output 
+	# $t0 = how many dict items were written
+	# $t1 = temporary address holder
+	# $t2 = current dict index
+	# $t3 = current char
+	# $t4 = temporary char/int holder
+	writeDictFile:
+		addi $sp, $sp, -8 # writeDictFile
+		sw $s0, 4($sp)
+		sw $s1, 8($sp)
+		
+		lw $s0, outputDict
+		lw $s1, outputChars
+		fopen FP, outputDictFileName, 1
+		move $t0, $zero
+		writeDict:
+			add $t1, $s1, $t0
+			lb  $t3, ($t1)
+			sll $t1, $t0, 2
+			add $t1, $s0, $t1
+			lw  $t2, ($t1)
+			beq $t2, -1, writeBreak
+			pint $t0
+			intwrite $t0, FP
+			pcharm colon
+			fwrite colon, 1, FP
+			pcharm space
+			fwrite space, 1, FP
+			pint $t2
+			intwrite $t2, FP
+			pcharm comma
+			fwrite comma, 1, FP
+			pcharm space
+			fwrite space, 1, FP
+			pchar $t3
+			sb  $t3, outputCharBuffer
+			fwrite outputCharBuffer, 1, FP
+			newl
+			fwrite newline, 1, FP
+			addi $t0, $t0, 1
+			j writeDict
+		writeBreak:		
+		lw $s1, 8($sp)
+		lw $s0, 4($sp)
+		addi $sp, $sp, +8
+		jr $ra
 
 	countBytes:
 		addi $sp, $sp, -4
 		sw $s0, 0($sp)
-		fread countBuffer, 1024, inputFP
+		fread countBuffer, 1024, FP
 		beqz $v0, countBreak
 		add $s0, $s0, $v0
 		j countBytes
@@ -71,16 +125,16 @@
 		lw $s0, outputDict
 		lw $s1, outputChars
 		whileCompress:
-			fread nextChar, 1, inputFP
+			fread nextChar, 1, FP
 			beqz $v0, compressBreak
 			lb $t2, nextChar
 			move $t1, $zero
 			move $t3, $zero
 			whileDict:
-				beq $t0, $t1, dictBreak 
+				beq $t0, $t1, dictBreak #test if you finished testing current dict (if so, jump to dictBreak)
 				sll $t4, $t1, 2
 				add $t4, $s0, $t4
-				lw  $t4, ($t4)
+				lw  $t4, ($t4)          #load next dict index entry 
 				beq $t3, $t4, dictEqual #if $t3 equals $t4 (current output index equals tested dict index)
 				addi $t1, $t1, 1
 				j whileDict	
@@ -93,11 +147,11 @@
 					j whileDict                 #go back to whileDict
 					charEqual:              #if so,
 					move $t3, $t1               #save t1 to $t3
-					fread nextChar, 1, inputFP  #load a new character from the file
+					fread nextChar, 1, FP  #load a new character from the file
 					beqz $v0, compressBreak     #test if EOF (if so jump to compressBreak)
 					lb $t2, nextChar
-					addi $t1, $t1, 1
-					j whileDict
+					addi $t1, $t1, 1	    #increment $t1
+					j whileDict                 #go back to whileDict
 			dictBreak:
 			pint  $t3
 			pchar $t2
@@ -110,7 +164,13 @@
 			j whileCompress
 		compressBreak:
 		newl
-		sw $s1, 8($sp)
-		sw $s0, 4($sp)
+		addi $t0, $t0, 1
+		sll  $t6, $t0, 2
+		add  $t6, $s0, $t6
+		li   $t3, -1
+		sw   $t3, ($t6)
+		
+		lw $s1, 8($sp)
+		lw $s0, 4($sp)
 		addi $sp, $sp, +8
 		jr $ra
