7c4706b16a9b64fe779d9527a68b22065056a66a
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-06-21 02:38:13 -0300

    lw hazard (stall) now working
    also a lot of tiny bug fixes and workarounds

--- pipeline/Const2.vhd
+++ pipeline/Const2.vhd
@@ -1,12 +1,12 @@
--- megafunction wizard: %LPM_MUX%
+-- megafunction wizard: %LPM_CONSTANT%
 -- GENERATION: STANDARD
 -- VERSION: WM1.0
--- MODULE: LPM_MUX 
+-- MODULE: LPM_CONSTANT 
 
 -- ============================================================
--- File Name: Mux2_5bits.vhd
+-- File Name: Const2.vhd
 -- Megafunction Name(s):
--- 			LPM_MUX
+-- 			LPM_CONSTANT
 --
 -- Simulation Library Files(s):
 -- 			lpm
@@ -37,57 +37,45 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 LIBRARY lpm;
-USE lpm.lpm_components.all;
+USE lpm.all;
 
-ENTITY Mux2_5bits IS
+ENTITY Const2 IS
 	PORT
 	(
-		data0x		: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
-		data1x		: IN STD_LOGIC_VECTOR (4 DOWNTO 0);
-		sel		: IN STD_LOGIC ;
 		result		: OUT STD_LOGIC_VECTOR (4 DOWNTO 0)
 	);
-END Mux2_5bits;
+END Const2;
 
 
-ARCHITECTURE SYN OF mux2_5bits IS
-
---	type STD_LOGIC_2D is array (NATURAL RANGE <>, NATURAL RANGE <>) of STD_LOGIC;
+ARCHITECTURE SYN OF const2 IS
 
 	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (4 DOWNTO 0);
-	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (4 DOWNTO 0);
-	SIGNAL sub_wire2	: STD_LOGIC_2D (1 DOWNTO 0, 4 DOWNTO 0);
-	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (4 DOWNTO 0);
-	SIGNAL sub_wire4	: STD_LOGIC ;
-	SIGNAL sub_wire5	: STD_LOGIC_VECTOR (0 DOWNTO 0);
+
+
+
+	COMPONENT lpm_constant
+	GENERIC (
+		lpm_cvalue		: NATURAL;
+		lpm_hint		: STRING;
+		lpm_type		: STRING;
+		lpm_width		: NATURAL
+	);
+	PORT (
+			result	: OUT STD_LOGIC_VECTOR (4 DOWNTO 0)
+	);
+	END COMPONENT;
 
 BEGIN
-	sub_wire3    <= data0x(4 DOWNTO 0);
 	result    <= sub_wire0(4 DOWNTO 0);
-	sub_wire1    <= data1x(4 DOWNTO 0);
-	sub_wire2(1, 0)    <= sub_wire1(0);
-	sub_wire2(1, 1)    <= sub_wire1(1);
-	sub_wire2(1, 2)    <= sub_wire1(2);
-	sub_wire2(1, 3)    <= sub_wire1(3);
-	sub_wire2(1, 4)    <= sub_wire1(4);
-	sub_wire2(0, 0)    <= sub_wire3(0);
-	sub_wire2(0, 1)    <= sub_wire3(1);
-	sub_wire2(0, 2)    <= sub_wire3(2);
-	sub_wire2(0, 3)    <= sub_wire3(3);
-	sub_wire2(0, 4)    <= sub_wire3(4);
-	sub_wire4    <= sel;
-	sub_wire5(0)    <= sub_wire4;
 
-	LPM_MUX_component : LPM_MUX
+	LPM_CONSTANT_component : LPM_CONSTANT
 	GENERIC MAP (
-		lpm_size => 2,
-		lpm_type => "LPM_MUX",
-		lpm_width => 5,
-		lpm_widths => 1
+		lpm_cvalue => 2,
+		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
+		lpm_type => "LPM_CONSTANT",
+		lpm_width => 5
 	)
 	PORT MAP (
-		data => sub_wire2,
-		sel => sub_wire5,
 		result => sub_wire0
 	);
 
@@ -99,24 +87,23 @@ END SYN;
 -- CNX file retrieval info
 -- ============================================================
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
+-- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
+-- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
+-- Retrieval info: PRIVATE: Radix NUMERIC "10"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
+-- Retrieval info: PRIVATE: Value NUMERIC "2"
+-- Retrieval info: PRIVATE: nBit NUMERIC "5"
 -- Retrieval info: PRIVATE: new_diagram STRING "1"
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
--- Retrieval info: CONSTANT: LPM_SIZE NUMERIC "2"
--- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_MUX"
+-- Retrieval info: CONSTANT: LPM_CVALUE NUMERIC "2"
+-- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
+-- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_CONSTANT"
 -- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "5"
--- Retrieval info: CONSTANT: LPM_WIDTHS NUMERIC "1"
--- Retrieval info: USED_PORT: data0x 0 0 5 0 INPUT NODEFVAL "data0x[4..0]"
--- Retrieval info: USED_PORT: data1x 0 0 5 0 INPUT NODEFVAL "data1x[4..0]"
 -- Retrieval info: USED_PORT: result 0 0 5 0 OUTPUT NODEFVAL "result[4..0]"
--- Retrieval info: USED_PORT: sel 0 0 0 0 INPUT NODEFVAL "sel"
--- Retrieval info: CONNECT: @data 1 0 5 0 data0x 0 0 5 0
--- Retrieval info: CONNECT: @data 1 1 5 0 data1x 0 0 5 0
--- Retrieval info: CONNECT: @sel 0 0 1 0 sel 0 0 0 0
 -- Retrieval info: CONNECT: result 0 0 5 0 @result 0 0 5 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL Mux2_5bits.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Mux2_5bits.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Mux2_5bits.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Mux2_5bits.bsf TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Mux2_5bits_inst.vhd FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Const2.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Const2.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Const2.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Const2.bsf TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Const2_inst.vhd FALSE
 -- Retrieval info: LIB_FILE: lpm
